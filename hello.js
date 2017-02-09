303fc2469f2052945783b85d1a6b66b7521ad76b
dexter aparicio <syscools@gmail.com>
2017-02-10 04:45:19 +0800

    adjust for bluemix

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var cfenv = require('cfenv');
 var app = express();
 //var CircularJSON = require('circular-json');
+var appEnv = cfenv.getAppEnv();
 
 app.get('/',function (req,res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -8,6 +10,6 @@ app.get('/',function (req,res) {
     res.send('Hello World');
 });
 
-app.listen(30000,"0.0.0.0",function () {
-    console.log('Example app listening on port 30000');
+app.listen(appEnv.port,"0.0.0.0",function () {
+    console.log('Example app listening on port ' + appenv.url);
 }); 
